7e66a6c4e3cf384cf1fc7b41f6f2bb137bf22046
tarry2009 <tarry2009@users.noreply.github.com>
2017-08-08 17:48:41 +0500

    Update api.php

--- routes/api.php
+++ routes/api.php
@@ -15,19 +15,5 @@ use Illuminate\Http\Request;
 
  Route::group(['middleware' => ['api','cors'],'prefix' => 'v1'], function () {
 //Route::group(['prefix' => 'v1'], function(){
-	
-		Route::get('events', 'EventController@events');
-		Route::get('stands', 'EventController@stands');
-		Route::get('test', 'EventController@testx');
-	
-		
-		
-		Route::get('test', 'EventController@testx');
-		Route::post('test', 'EventController@testx');
-		Route::delete('test', 'EventController@testx');
-		Route::put('test/{id}', 'EventController@testx');
-		Route::put('test', 'EventController@testx');
-		 
-		//Route::get('search', ['middleware' => 'cors'],'UserController@search');
- 
+	    
 });
